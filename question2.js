01874c3358387ca7e3039247b1cb50bdc4b1c302
Sujithhh <117425622+Sujithhh@users.noreply.github.com>
2025-11-25 18:15:26 +0000

    Add function to print even numbers between 1 to 20 using for and while loops

---
+++ question2.js
@@ -0,0 +1,21 @@
+// print even numbers between 1 to 20 
+
+
+function even(n){
+    for(let i=0;i<=n;i=i+2){
+        console.log(i)
+    }
+}
+even(20)
+
+// same using while loop
+
+function even(n){
+    let i = 0;
+    while( i <= n){
+        console.log(i)
+        i=i+2
+    }
+}
+even(20)
+
