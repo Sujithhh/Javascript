01441732b9838f91617c2564e58333271d2d8f3e
Sujithhh <117425622+Sujithhh@users.noreply.github.com>
2025-11-25 18:07:32 +0000

    first commit

---
+++ question1.js
@@ -0,0 +1,21 @@
+// Print numbers from 1 to 10
+
+
+function Numbers(n){
+    for(let i =1 ;i<=n;i++){
+        console.log(i)
+    }
+}
+Numbers(10)
+
+
+// same using while loop 
+
+function Numbers(n){
+    let i=1;
+    while(i<=n){
+        console.log(i);
+        i++;
+    }
+}
+Numbers(10)
